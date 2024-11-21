3b7eb1faa3c1598cd11214f145b6b00d54c81073
n3ohck.mx <alexasa.wdc@gmail.com>
2024-11-21 13:30:19 -0600

    feature: se retira el descuento y suma de existencia sobre productos tour y tour_paquete

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
                 return response()->json(['message' => 'Los datos ingresados son invalidos, por favor intenta nuevamente.'], 400);
             }
 
-            if ( ( !$user->hasRole('APP USER') || !$user->hasRole('CONSULTA') )) {
+            if (!$user->hasRole('APP USER') || !$user->hasRole('CONSULTA')) {
                 return response()->json(['message' => 'Este usuario no tiene permisos para acceder al sistema'], 400);
             }
         } catch (JWTException $e) {
