d5603d165df248db53b35c2adab292318e54bafc
alexsalgado <n3ohck.mx@gmail.com>
2025-06-23 19:10:01 -0600

    hotfix: soporte de venta de paquetes

--- app/Http/Controllers/Admin/PaymentReportController.php
+++ app/Http/Controllers/Admin/PaymentReportController.php
@@ -81,12 +81,13 @@ class PaymentReportController extends Controller
             ->get()
             ->map(function($payment){
                 return [
+                    'id' => $payment->id,
                     'type' => 'Pago Empleado',
                     'amount' => $payment->monto,
                     'date' => Carbon::parse($payment->created_at)->format('Y-m-d H:i:s'),
-                    'user' => ( !isset( $employee->empleado ) ) ? 'N/A' : $employee->empleado->nombres . ' ' . $employee->empleado->apellidos,
-                    'branch' =>  ( !isset( $employee->empleado ) ) ? 'N/A' : $employee->empleado->sucursal->razon_social,
-                    'image' => asset('storage/pagos/' . $employee->imagen)
+                    'user' => ( !isset( $employee->empleado ) ) ? 'N/A' : $payment->empleado->nombres . ' ' . $payment->empleado->apellidos,
+                    'branch' =>  ( !isset( $payment->empleado ) ) ? 'N/A' : $payment->empleado->sucursal->razon_social,
+                    'image' => asset('storage/pagos/' . $payment->imagen)
                 ];
             });
     }
