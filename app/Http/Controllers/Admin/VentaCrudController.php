d53826dd920a2efb0003b2f792ed0418ddc504b6
alexsalgado <n3ohck.mx@gmail.com>
2025-07-30 09:13:48 -0600

    feature: soportar los descuentos como los dejo kaleb en ventas online

--- app/Http/Controllers/Admin/VentaCrudController.php
+++ app/Http/Controllers/Admin/VentaCrudController.php
@@ -429,7 +429,10 @@ class VentaCrudController extends CrudController
                 ->get()
                 ->map(function ($producto) {
                     $paymentsOnline = $producto->venta->pagos->where('tipo', 'online')->count();
-                    dd($paymentsOnline,$producto->precio, $producto->descuento, $producto->porcentaje_descuento,$producto->total);
+                    if ($paymentsOnline){
+                        $producto->descuento = $producto->total - $producto->descuento;
+                    }
+                    //dd($paymentsOnline,$producto->precio, $producto->descuento, $producto->porcentaje_descuento,$producto->total);
                     return [
                         'fecha' => $producto->created_at->format('Y-m-d H:i:s'),
                         'producto' => $producto->producto->descripcion,
