1a3cc2a2a8bc3abd4d5832b0fb48194ac8821fdf
alexsalgado <n3ohck.mx@gmail.com>
2025-09-13 09:30:49 -0600

    hotfix: fixing datetime

--- app/Models/Venta.php
+++ app/Models/Venta.php
@@ -128,7 +128,7 @@ class Venta extends Model
             $this->search->end_date = $this->toUtcForQuery($this->search->end_date, true);
         }
 
-        dd($this->search->start_date->setTimezone('America/Chihuahua')->format('Y-m-d H:i:s'));
+        dd(Carbon::parse($this->search->start_date, config('app.display_timezone', 'America/Chihuahua')));
 
         return $query
             ->when($this->search->folio, fn ($q) => $q->where('folio', $this->search->folio))
