e882d197a446492c79084903dacaa23fdeee15b6
alexsalgado <n3ohck.mx@gmail.com>
2025-09-13 09:26:40 -0600

    hotfix: fixing datetime

--- app/Models/Venta.php
+++ app/Models/Venta.php
@@ -122,7 +122,7 @@ class Venta extends Model
 
         if ($this->search->start_date) {
             $this->search->start_date = $this->toUtcForQuery($this->search->start_date, false);
-            // dd($this->search->start_date->copy()->setTimezone(config('app.display_timezone'))); // Para ver cómo queda en tu hora local
+            dd($this->search->start_date); // Para ver cómo queda en tu hora local
         }
 
         if ($this->search->end_date) {
