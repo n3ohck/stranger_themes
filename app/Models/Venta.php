e931a743108242dbb958a213dc8f49bcd11c731a
alexsalgado <n3ohck.mx@gmail.com>
2025-09-13 09:42:39 -0600

    hotfix: fixing datetime

--- app/Models/Venta.php
+++ app/Models/Venta.php
@@ -147,7 +147,7 @@ class Venta extends Model
 
         if ($this->search->start_date) {
             $this->search->start_date = $this->toUtcForQuery($this->search->start_date, false);
-
+            dd($this->search->start_date);
         }
 
         if ($this->search->end_date) {
