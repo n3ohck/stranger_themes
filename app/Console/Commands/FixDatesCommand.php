1472ffe1af4daf34a92c6ecb9b006d1f4b1401c8
alexsalgado <n3ohck.mx@gmail.com>
2025-09-20 15:08:22 -0600

    hotfix: fixing datetime

--- app/Console/Commands/FixDatesCommand.php
+++ app/Console/Commands/FixDatesCommand.php
@@ -45,7 +45,9 @@ class FixDatesCommand extends Command
                 $toZone = Carbon::parse($venta->created_at)
                     ->setTimezone( config('app.display_timezone', 'America/Chihuahua') )
                     ->format('Y-m-d H:i:s');
-                dd($venta->created_at, $toZone);
+                if ($venta->created_at !== $toZone) {
+                    dd($venta->created_at, $toZone);
+                }
             });
     }
 }
