444988c49b93eb0e0970c66c7f7dce5c2a1dd8e3
alexsalgado <n3ohck.mx@gmail.com>
2026-05-21 11:34:10 -0600

    hotfix: dupllicado de venta online

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,6 +57,12 @@ class User extends Authenticatable implements JWTSubject
         'email_verified_at' => 'datetime',
     ];
 
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
     protected $appends  = ['nombre_completo'];
 
     public function sucursal():BelongsTo
